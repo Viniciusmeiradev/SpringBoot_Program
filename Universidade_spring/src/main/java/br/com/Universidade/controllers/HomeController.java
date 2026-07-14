afa2e5d8956f43f6fa5147fe9b99116305dd5434
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-07-14 19:03:07 -0300

    Add files via upload

---
+++ Universidade_spring/src/main/java/br/com/Universidade/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package br.com.Universidade.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController{
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        mv.addObject("msg", "Mensagem do controler");
+        return mv;
+    }
+}   
---
+++ Universidade_spring/src/test/java/br/com/Universidade/UniversidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Universidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

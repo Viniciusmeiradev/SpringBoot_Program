be1a00a338ca524cc5b82cc15a4fa591c5cef5bf
Viniciusmeiradev <viniciusmeirapessoal@gmail.com>
2026-07-25 20:27:09 +0000

    alterar/excluir

--- Universidade_spring/src/main/java/br/com/Universidade/controllers/AlunoController.java
+++ Universidade_spring/src/main/java/br/com/Universidade/controllers/AlunoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import br.com.Universidade.model.Aluno;
 import br.com.Universidade.dao.AlunoDao;
 
@@ -25,8 +26,39 @@ public class AlunoController {
     @PostMapping("InsertAlunos")
     public ModelAndView inserirAluno(Aluno aluno){
         ModelAndView mv= new ModelAndView();
-        mv.setViewName("redirect:/Aluno/listAlunos");
+        mv.setViewName("redirect:/alunos-adicionados");
         alunorepositorio.save(aluno);
         return mv;
+    }
+
+    @GetMapping("alunos-adicionados")
+    public ModelAndView listagemAlunos(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("Aluno/listAlunos");
+        mv.addObject("alunosList", alunorepositorio.findAll());
+        return mv;
+    }
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Integer id){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("alterar");
+        Aluno aluno = alunorepositorio.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id));
+        mv.addObject("aluno", aluno);
+        return mv;
+    }
+
+    @PostMapping("alterar")
+    public ModelAndView alterar(Aluno aluno){
+        ModelAndView mv = new ModelAndView();
+        alunorepositorio.save(aluno);
+        mv.setViewName("redirect:/alunos-adicionados");
+        return mv;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirAluno(@PathVariable("id") Integer id){
+        alunorepositorio.deleteById(id);
+        return "redirect:/alunos-adicionados";
     }
 }
